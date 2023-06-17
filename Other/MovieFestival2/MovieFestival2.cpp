65f8db289dfe136f32b5b7b52e142ba2f3435a9f
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2023-06-17 00:01:38 -0500

    Tried Movie Festival 2 using a multiset

--- Other/MovieFestival2/MovieFestival2.cpp
+++ Other/MovieFestival2/MovieFestival2.cpp
@@ -4,11 +4,12 @@ using namespace std;
 int main(){
     int n, k;
     cin >> n >> k;
-    vector<pair<int, int>> movies(n); // sorted by end time, start time
+    multiset<pair<int, int>> movies; // sorted by end time, start time
     for(int i = 0; i < n; i++){
-        cin >> movies[i].second >> movies[i].first;
+        int a, b;
+        cin >> a >> b;
+        movies.insert({b, a});
     }
-    sort(movies.begin(), movies.end());
     int ans = 0;
     multiset<int> members;
     while(k--) members.insert(0);
