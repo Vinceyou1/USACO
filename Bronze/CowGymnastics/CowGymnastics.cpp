3dcade60e86399ba74287148db36aa93ce15fd5c
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2023-06-28 17:49:15 -0500

    Solved Cow Gymnastics
    http://www.usaco.org/index.php?page=viewproblem2&cpid=963

--- Bronze/CowGymnastics/CowGymnastics.cpp
+++ Bronze/CowGymnastics/CowGymnastics.cpp
@@ -2,41 +2,32 @@
 using namespace std;
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     freopen("gymnastics.in", "r", stdin);
     freopen("gymnastics.out", "w", stdout);
-
+    
     int k, n;
     cin >> k >> n;
-    vector<pair<int, int>> pairs;
-    for (int c = 0; c < k; c++){
-        int *arr = new int[n];
-        vector<pair<int, int>> temp;
-        vector<pair<int, int>> temp2;
-        for(int i = 0; i < n; i++){
-            cin >> arr[i];
+    int positions[11][21]; // indexed by session, then by cow
+    for(int i = 1; i <= k; i++){
+        for(int j = 1; j <= n; j++){
+            int c; cin >> c;
+            positions[i][c] = j;
         }
-        for(int i = 0; i < n; i++){
-            for(int i2 = i+1; i < n; i++){
-                temp.push_back(make_pair(arr[i], arr[i2]));
-            }
-        }
-        if (c == 0){
-            pairs = temp;
-        }
-        else {
-            for (pair<int, int> pair:temp){
-                if(find(pairs.begin(), pairs.end(), pair) == pairs.end()){
-                    temp2.push_back(pair);
+    }
+    int pairs = 0;
+    for(int i = 1; i <= n - 1; i++){
+        for(int j = i + 1; j <= n; j++){
+            bool consistent = true;
+            bool i_less_j = positions[1][i] < positions[1][j];
+            for(int session = 2; session <= k; session++){
+                if(i_less_j != (positions[session][i] < positions[session][j])){
+                    consistent = false;
+                    break;
                 }
             }
-            pairs = temp2;
+            if(consistent) {pairs++;}
         }
-        cout << pairs.size();
     }
-    cout << pairs.size();
-
+    cout << pairs;
     return 0;
 }
