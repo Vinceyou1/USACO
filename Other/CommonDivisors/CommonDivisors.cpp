b03548c339e8559a26f6d98e1a1ab0fad3795a5d
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2023-07-17 14:02:02 -0500

    Solved Common Divisors
    https://cses.fi/problemset/task/1081/
    Realized I could just use an array instead of a map
    For some reason still TLE on one of the test cases

--- Other/CommonDivisors/CommonDivisors.cpp
+++ Other/CommonDivisors/CommonDivisors.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int factors[1000001];
+
 int main(){
     ios_base::sync_with_stdio(0);
 	cin.tie(0);
@@ -8,17 +10,17 @@ int main(){
     int n; cin >> n;
     vector<int> nums(n);
     for(auto &i: nums) cin >> i;
-    int curr_max_factor = 1;
-    set<int> factors;
     for(int i = 0; i < n; i++){
         for(int f = 1; f*f <= nums[i]; f++){
             if(nums[i] % f == 0){
-                if(!factors.insert(f).second) curr_max_factor = max(f, curr_max_factor);
-                if(f*f != nums[i]){
-                    if(!factors.insert(nums[i] / f).second) curr_max_factor = max(nums[i] / f, curr_max_factor);
-                }
+                factors[f]++;
+                if(f != nums[i] / f) factors[nums[i] / f]++;
             }
         }
     }
-    cout << curr_max_factor;
+    int ans = 1;
+    for(int i = 1; i < 1000001; i++){
+        if(factors[i] >= 2) ans = i;
+    }
+    cout << ans;
 }
