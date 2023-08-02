4f3f2c48d5a1c4cf67eb7464701ca72c74b35dda
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2023-08-01 23:57:50 -0500

    Solved Cow Frisbee
    USACO 2022 January Contest, Silver

--- Silver/CowFrisbee/CowFrisbee.cpp
+++ Silver/CowFrisbee/CowFrisbee.cpp
@@ -2,33 +2,30 @@
 using namespace std;
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int n;
-    cin >> n;
-    int *nums = new int[n];
-
-    for(int c = 0; c < n; c++){
-        cin >> nums[c];
+    int n; cin >> n;
+    vector<int> nums(n);
+    for(int &i: nums) cin >> i;
+    map<int, int> height_to_pos;
+    for(int i = 0; i < n; i++){
+        height_to_pos[nums[i]] = i + 1;
     }
-    long long total = 0;
-    for(int c = 0; c < n; c++){
-        int value = nums[c];
-        int min = 0;
-        int c1 = c+1;
-        if (c1 == n) continue;
-        while(nums[c1] < value){
-            if(nums[c1] > min){
-                total += c1-c+1;
-                min = nums[c1];
-                
-            }
-            if(++c1==n) break;
-        }
-        if (c1 != n) {
-            total+=c1-c+1;
+    long long ans = 0;
+    set<int> positions;
+    for(int height = n; height > 0; height--){
+        auto it = positions.insert(height_to_pos[height]).first;
+        if(positions.size() == 1) continue;
+        if(it == positions.begin()){
+            it++;
+            ans += (*it) - height_to_pos[height] + 1; 
+        } else if(it == --(positions.end())){
+            it--;
+            ans += height_to_pos[height] - (*it) + 1;
+        } else {
+            it++;
+            ans += (*it) - height_to_pos[height] + 1; 
+            it--; it--;
+            ans += height_to_pos[height] - (*it) + 1;
         }
     }
-    cout << total;
-    return 0;
+    cout << ans;
 }
