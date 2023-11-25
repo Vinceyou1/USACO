10e5c262e224abd3cf52329d0aa9588229552963
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2023-11-24 18:42:20 -0600

    Fixed Common Divisors
    https://cses.fi/problemset/task/1081/

--- CSES/CommonDivisors/CommonDivisors.cpp
+++ CSES/CommonDivisors/CommonDivisors.cpp
@@ -10,17 +10,19 @@ int main(){
     int n; cin >> n;
     vector<int> nums(n);
     for(auto &i: nums) cin >> i;
-    for(int i = 0; i < n; i++){
-        for(int f = 1; f*f <= nums[i]; f++){
+    for(int i = 0; i < n; ++i){
+        int upper = (int) sqrt(nums[i]);
+        for(int f = 1; f <= upper; ++f){
             if(nums[i] % f == 0){
                 factors[f]++;
                 if(f != nums[i] / f) factors[nums[i] / f]++;
             }
         }
     }
-    int ans = 1;
-    for(int i = 1; i < 1000001; i++){
-        if(factors[i] >= 2) ans = i;
+    for(int i = 1000000; i >= 1; --i){
+        if(factors[i] >= 2) {
+            cout << i;
+            return 0;
+        }
     }
-    cout << ans;
 }
